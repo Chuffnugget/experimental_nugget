7f944047541ae440011f4e669b99128a87c18a4f
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2025-03-13 22:53:31 +0000

    Update __init__.py

--- custom_components/experimental_nugget/__init__.py
+++ custom_components/experimental_nugget/__init__.py
@@ -1,1 +1,25 @@
 #__init__.py
+"""Initialize the experiemtental_nugget integration."""
+import logging
+
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+
+_LOGGER = logging.getLogger(__name__)
+DOMAIN = "experiemtental_nugget"
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the integration from configuration.yaml (if any)."""
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up the integration from a config entry."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return True
