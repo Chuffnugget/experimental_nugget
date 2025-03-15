e9563112651470c45b6e092020b76e547c118124
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2025-03-15 16:12:46 +0000

    Update config_flow.py

--- custom_components/experimental_nugget/config_flow.py
+++ custom_components/experimental_nugget/config_flow.py
@@ -4,7 +4,6 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.data_entry_flow import FlowResult
-from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
@@ -15,14 +14,14 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input: dict | None = None) -> FlowResult:
-        _LOGGER.debug("Entered async_step_user with user_input: %s", user_input)
+        _LOGGER.debug("async_step_user called with: %s", user_input)
         if user_input is not None:
             _LOGGER.info("User provided input: %s", user_input)
-            _LOGGER.info("Creating config entry for Experimental Nugget")
+            _LOGGER.info("Creating config entry for %s", DOMAIN)
             return self.async_create_entry(title="Experimental Nugget", data={})
-        _LOGGER.debug("Showing form for async_step_user")
+        _LOGGER.debug("Showing form in async_step_user")
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({}),
-            description_placeholders={"info": "Provide input if required."}
+            description_placeholders={"info": "No configuration needed."}
         )
